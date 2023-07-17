563a61b8654e3c13e14a42f5b75be9a5360edee1
elvisboyf <77184979+elvisboyf@users.noreply.github.com>
2023-07-17 18:56:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from binance.client import Client
-client = Client("b1m4F6maj9cChCOUEo5gkcGnkgfC9gSjeivju245a51t71GZVYjza0eZHJEd8tsa","AbQ8BWY2WbQXkAJt63binouleSPZFKjQXcvKrBlbThArKq55O2vY1jhhjbTXvLbI")
-
-orders = client.futures_position_information(symbol="OCEANUSDT")
-print(orders[1])
-for orden in orders:
-    print(orden)
+from flask import Flask, request, make_response
+app = Flask(__name__)
+@app.route('/trading-signal', methods=['POST'])
+def receive_trading_signal():
+    return "Hola Cabrom"
