7e2f1e309cdc3d14be8dc1b64ea5418fe9a20340
elvisboyf <77184979+elvisboyf@users.noreply.github.com>
2023-07-17 18:41:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-from flask import Flask, request
-app = Flask(__name__)
-@app.route('/', methods=['GET','POST'])
-def home():
-    return "HOLA"
-
-
-if __name__ == '__main__':
-    app.run()
-    #ngrok http --domain=carefully-striking-snail.ngrok-free.app 80
+return "hola"
