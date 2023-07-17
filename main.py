856e8bf20d41ee80c42fb3700d9d757bd280fec3
elvisboyf <77184979+elvisboyf@users.noreply.github.com>
2023-07-17 18:46:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hola")
+from binance.client import Client
+client = Client("b1m4F6maj9cChCOUEo5gkcGnkgfC9gSjeivju245a51t71GZVYjza0eZHJEd8tsa","AbQ8BWY2WbQXkAJt63binouleSPZFKjQXcvKrBlbThArKq55O2vY1jhhjbTXvLbI")
+
+orders = client.futures_position_information(symbol="OCEANUSDT")
+print(orders[1])
+for orden in orders:
+    print(orden)
