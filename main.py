70606c68d426798f2679394bb1c64337c04869c2
elvisboyf <77184979+elvisboyf@users.noreply.github.com>
2023-07-24 00:35:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-from flask import Flask, request, make_response
-app = Flask(__name__)
-@app.route('/trading-signal', methods=['POST'])
-def receive_trading_signal():
-    return "Hola Cabrom"
-
-if __name__ == '__main__':
-    app.run()
+print("hola cabron")
